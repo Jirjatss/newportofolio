6919ddba257df17b61fae462d8a1df2da5f1f5fb
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-10-30 01:50:48 +0800

    atur responsive

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -30,44 +30,46 @@ function Contact() {
       <div className="mb-10 text-center">
         <span className="border-b-4 border-[#6419e6]">Contact</span>
       </div>
-      <div className="grid grid-cols-1 w-96 mx-auto">
+      <div className="grid grid-cols-1 mx-auto">
         <form>
-          <div className="mb-6">
+          <div className="mb-6 lg:w-96 md:w-96 sm:w-10 mx-auto">
             <input
               type="text"
               id="name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block lg:w-full md:w-full sm:w-3/4 p-2.5  mx-auto"
               placeholder="Your Name"
               required=""
               onChange={(e) => setNama(e.target.value)}
             />
           </div>
-          <div className="mb-6">
+          <div className="mb-6 lg:w-96 md:w-96 sm:w-10 mx-auto">
             <input
               type="email"
               id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block lg:w-full md:w-full sm:w-3/4 p-2.5 mx-auto"
               placeholder="Your Email"
               required=""
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-          <div className="flex items-start mb-6">
+          <div className="flex items-start mb-6 lg:w-96 md:w-96 sm:w-10 mx-auto">
             <textarea
               id="message"
               rows="4"
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 lg:w-full md:w-full sm:w-3/4 mx-auto"
               placeholder="Leave a message for me"
               onChange={(e) => setPesan(e.target.value)}
             ></textarea>
           </div>
-          <button
-            onClick={formhandle}
-            type="submit"
-            className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-[#6419e6] rounded-lg border border-gray-300 hover:bg-[#4f15b9] focus:ring-4 focus:outline-none focus:ring-gray-200"
-          >
-            Submit
-          </button>
+          <div className="flex items-start mb-6 lg:w-96 md:w-96 sm:w-10 mx-auto">
+            <button
+              onClick={formhandle}
+              type="submit"
+              className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-[#6419e6] rounded-lg border border-gray-300 hover:bg-[#4f15b9] focus:ring-4 focus:outline-none focus:ring-gray-200 mx-auto"
+            >
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </div>

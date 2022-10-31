2d5334f64c5aea8471e055c3acd8c506e30078cd
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-10-31 22:20:28 +0800

    setopen

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -42,22 +42,22 @@ function Navbar() {
           <div className={open ? "hidden w-full md:block md:w-auto " : "w-full md:block md:w-auto "} id="navbar-default">
             <ul className="flex  flex-col lg:h-10 p-4 mt-4 mb-4 bg-transparent rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 font-signika">
               <li>
-                <HashLink to="/#about" className="block  pr-4 pl-3 text-gray-700 rounded  md:border-0 0 md:p-0" smooth>
+                <HashLink to="/#about" className="block  pr-4 pl-3 text-gray-700 rounded  md:border-0 0 md:p-0" smooth onClick={() => setOpen(!open)}>
                   About
                 </HashLink>
               </li>
               <li>
-                <HashLink to="/#education" className="block  pr-4 pl-3 text-gray-700 rounded  md:border-0 0 md:p-0" smooth>
+                <HashLink to="/#education" className="block  pr-4 pl-3 text-gray-700 rounded  md:border-0 0 md:p-0" smooth onClick={() => setOpen(!open)}>
                   Education
                 </HashLink>
               </li>
               <li>
-                <HashLink to="/#skills" className="block  pr-4 pl-3 text-gray-700 rounded  md:border-0 0 md:p-0" smooth>
+                <HashLink to="/#skills" className="block  pr-4 pl-3 text-gray-700 rounded  md:border-0 0 md:p-0" smooth onClick={() => setOpen(!open)}>
                   Skills
                 </HashLink>
               </li>
               <li>
-                <HashLink to="/#contact" className="block  pr-4 pl-3 text-gray-700 rounded  md:border-0 0 md:p-0" smooth>
+                <HashLink to="/#contact" className="block  pr-4 pl-3 text-gray-700 rounded  md:border-0 0 md:p-0" smooth onClick={() => setOpen(!open)}>
                   Contact
                 </HashLink>
               </li>
